146c85c0610732e8e55551dcd2e85da721e0a5c3
Donghyuk.Yang <sency90@gmail.com>
2025-07-30 23:58:49 +0900

    [feature] ssd 명령어 기본기능 1차 구현

--- SSD/SSD.cpp
+++ SSD/SSD.cpp
@@ -1,5 +1,10 @@
 #include "SSD.h"
 
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+
 SSD::SSD() { Clear(); }
 void SSD::Clear() {
   storage.clear();
@@ -13,7 +18,15 @@ void SSD::SetReadCmd(ICmd *cmd) { this->readCmd = cmd; }
 // unsigned int SSD::Read(int lba) const { return readCmd.Run(lba, storage); }
 unsigned int SSD::Read(ReadArguments &readArgs) {
   readCmd->Run(&readArgs);
-  return dynamic_cast<ReadCmd *>(readCmd)->GetOutputData();
+  unsigned int readData = dynamic_cast<ReadCmd *>(readCmd)->GetOutputData();
+  std::ofstream ofs;
+  ofs.open("../SSD/ssd_output.txt");
+  ofs << " 0x" + (std::stringstream()
+                  << std::hex << std::setw(8) << std::setfill('0') << readData)
+                     .str();
+
+  ofs.close();
+  return readData;
 }
 
 void SSD::Write(IArguments &args) { writeCmd->Run(&args); }
