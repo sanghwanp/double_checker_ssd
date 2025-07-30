e805f23b0249333c87a71810f04dfae7afc0048f
Donghyuk.Yang <sency90@gmail.com>
2025-07-30 13:31:33 +0900

    [Refactoring] lba를 int -> unsigned int type으로 변경

--- SSD/ReadArguments.h
+++ SSD/ReadArguments.h
@@ -1,7 +1,7 @@
 #pragma once
+#include <cstring>
 #include <sstream>
 #include <string>
-#include <cstring>
 #include <vector>
 #define interface struct
 
@@ -9,7 +9,7 @@ class ReadArguments {
   enum CmdType { CMD_TYPE_OTHER, CMD_TYPE_WRITE, CMD_TYPE_READ };
 
  public:
-  int GetLba() const { return lba; }
+  unsigned int GetLba() const { return lba; }
 
   virtual ~ReadArguments() = default;
   void Parse(std::string cmdStr) {
@@ -17,18 +17,18 @@ class ReadArguments {
 
     std::vector<std::string> tokens;
     std::string token;
-    while(iss >> token) {
-        tokens.push_back(token);
+    while (iss >> token) {
+      tokens.push_back(token);
     }
-    if(tokens.size()!=2) {
-        throw std::invalid_argument("Invalid Arguments: argc must be 2");
+    if (tokens.size() != 2) {
+      throw std::invalid_argument("Invalid Arguments: argc must be 2");
     }
     std::string typeStr = tokens[0];
 
     if (typeStr == "R" || typeStr == "read") {
       cmdType = CMD_TYPE_READ;
       std::string lbaStr = tokens[1];
-      lba = std::stoi(lbaStr);
+      lba = std::stoul(lbaStr);
     }
 
     if (CheckErrorCmd()) {
@@ -38,7 +38,7 @@ class ReadArguments {
 
   bool CheckErrorCmd() {
     if (cmdType != CMD_TYPE_READ) return true;
-    if (lba < 0 || lba > MAX_LBA) return true;
+    if (lba > MAX_LBA) return true;
     return false;
   }
 
@@ -46,7 +46,7 @@ class ReadArguments {
 
  private:
   CmdType cmdType = CMD_TYPE_OTHER;  // 2: ReadCmdType
-  int lba;
+  unsigned int lba;
 
-  const int MAX_LBA = 99;
+  const unsigned int MAX_LBA = 99;
 };
