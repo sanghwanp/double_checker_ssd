6cbe96bedfcbd3ba08b1232c114447ae4d2789b8
Donghyuk.Yang <sency90@gmail.com>
2025-07-30 13:35:23 +0900

    [Refactoring] ReadArguments Class header와 cpp파일로 분리

--- SSD/ReadArguments.cpp
+++ SSD/ReadArguments.cpp
@@ -1,1 +1,35 @@
 #include "ReadArguments.h"
+
+ReadArguments::ReadArguments() {}
+
+ unsigned int ReadArguments::GetLba() const { return lba; }
+
+void ReadArguments::Parse(std::string cmdStr) {
+  std::istringstream iss(cmdStr);
+
+  std::vector<std::string> tokens;
+  std::string token;
+  while (iss >> token) {
+    tokens.push_back(token);
+  }
+  if (tokens.size() != 2) {
+    throw std::invalid_argument("Invalid Arguments: argc must be 2");
+  }
+  std::string typeStr = tokens[0];
+
+  if (typeStr == "R" || typeStr == "read") {
+    cmdType = CMD_TYPE_READ;
+    std::string lbaStr = tokens[1];
+    lba = std::stoul(lbaStr);
+  }
+
+  if (CheckErrorCmd()) {
+    throw std::invalid_argument("Invalid Arguments");
+  }
+}
+
+bool ReadArguments::CheckErrorCmd() {
+  if (cmdType != CMD_TYPE_READ) return true;
+  if (lba > MAX_LBA) return true;
+  return false;
+}
--- SSD/ReadArguments.h
+++ SSD/ReadArguments.h
@@ -9,44 +9,16 @@ class ReadArguments {
   enum CmdType { CMD_TYPE_OTHER, CMD_TYPE_WRITE, CMD_TYPE_READ };
 
  public:
-  unsigned int GetLba() const { return lba; }
-
+  ReadArguments();
   virtual ~ReadArguments() = default;
-  void Parse(std::string cmdStr) {
-    std::istringstream iss(cmdStr);
 
-    std::vector<std::string> tokens;
-    std::string token;
-    while (iss >> token) {
-      tokens.push_back(token);
-    }
-    if (tokens.size() != 2) {
-      throw std::invalid_argument("Invalid Arguments: argc must be 2");
-    }
-    std::string typeStr = tokens[0];
-
-    if (typeStr == "R" || typeStr == "read") {
-      cmdType = CMD_TYPE_READ;
-      std::string lbaStr = tokens[1];
-      lba = std::stoul(lbaStr);
-    }
-
-    if (CheckErrorCmd()) {
-      throw std::invalid_argument("Invalid Arguments");
-    }
-  }
-
-  bool CheckErrorCmd() {
-    if (cmdType != CMD_TYPE_READ) return true;
-    if (lba > MAX_LBA) return true;
-    return false;
-  }
-
-  ReadArguments() {}
+ public:
+  unsigned int GetLba() const;
+  void Parse(std::string cmdStr);
+  bool CheckErrorCmd();
 
  private:
   CmdType cmdType = CMD_TYPE_OTHER;  // 2: ReadCmdType
   unsigned int lba;
-
   const unsigned int MAX_LBA = 99;
 };
