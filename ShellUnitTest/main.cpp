89e9a298b7ead8fd162522de87b2d28da5b6ef53
sieumn <sieumn@gmail.com>
2025-07-30 13:44:25 +0900

    [bugfix] revert to the original main function

--- ShellUnitTest/main.cpp
+++ ShellUnitTest/main.cpp
@@ -4,12 +4,7 @@
 using namespace testing;
 
 int main() {
-  //InitGoogleMock();
-  //return RUN_ALL_TESTS();
-
   InitGoogleMock();
-  int result = RUN_ALL_TESTS();
-  std::cin.get();
-  return result;
+  return RUN_ALL_TESTS();
 }
 #endif
