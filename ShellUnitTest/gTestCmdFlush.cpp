429849e2aa615482c9412844450f155d87201e19
sieumn <sieumn@gmail.com>
2025-07-31 16:26:52 +0900

    [Refactoring] Remove comment

--- ShellUnitTest/gTestCmdFlush.cpp
+++ ShellUnitTest/gTestCmdFlush.cpp
@@ -14,11 +14,11 @@ class FlushTest : public Test {
 };
 
 TEST_F(FlushTest, FlushDefault_CallsFlushOnce) {
-  EXPECT_CALL(ssd, Flush()).Times(1);  // gMock으로 검증
+  EXPECT_CALL(ssd, Flush()).Times(1);
   EXPECT_TRUE(cmd.Call({"flush"}));
 }
 
 TEST_F(FlushTest, FlushBadArgs_NoFlush) {
-  EXPECT_CALL(ssd, Flush()).Times(0);  // 호출 없어야 함
+  EXPECT_CALL(ssd, Flush()).Times(0);
   EXPECT_FALSE(cmd.Call({"flush", "now"}));
 }
