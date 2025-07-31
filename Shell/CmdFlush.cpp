b5a1f61da2d7f5ae62d361ca13b3406d1380c13c
sieumn <sieumn@gmail.com>
2025-07-31 14:17:43 +0900

    [Refactoring] Apply Google code style

--- Shell/CmdFlush.cpp
+++ Shell/CmdFlush.cpp
@@ -2,15 +2,15 @@
 
 #include <iostream>
 
-CommandFlush::CommandFlush(SSDInterface* ssdInterface)
-    : ssd(ssdInterface) {}
+CommandFlush::CommandFlush(SSDInterface* ssdInterface) : ssd(ssdInterface) {}
 
 bool CommandFlush::Call(const std::vector<std::string>& program) {
   if (program.size() != 1) {
     printInvalidCommandMessage();
     return false;
-  } 
-   ssd->Flush();
+  }
+  ssd->Flush();
+  return true;
 }
 
 void CommandFlush::printInvalidCommandMessage() {
