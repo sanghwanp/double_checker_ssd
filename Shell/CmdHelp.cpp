5d5862528a3de1701dcd7ec957dce71f0b2f406b
shpark <zpxsch9001@naver.com>
2025-08-01 15:56:17 +0900

    [Refactoring] Change the help output to English
    The help output has been changed to English to prevent characters from
    being broken depending on the environment.

--- Shell/CmdHelp.cpp
+++ Shell/CmdHelp.cpp
@@ -9,21 +9,20 @@ typedef std::tuple<std::string, std::string, std::string> HelpString;
 
 namespace {
 static const std::vector<HelpString> helpString = {
-        {"write", "write [LBA] [Value]", "지정한 LBA에 값을 기록합니다."},
-        {"read", "read [LBA]", "지정한 LBA에서 값을 읽습니다."},
-        {"fullwrite", "fullwrite [Value]", "모든 LBA에 동일한 값을 기록합니다."},
-        {"fullread", "fullread", "모든 LBA를 읽고 출력합니다."},
-        {"erase", "erase [LBA]  [SIZE]", "특정LBA 부터 특정 Size까지 값을 삭제합니다."},
-        {"erase_range", "erase_range [Start LBA]  [End LBA]", "Start LBA 부터 End LBA 까지 값을 삭제합니다."},
-        {"flush", "flush", "Command Buffer에 있는 모든 명령어들을 수행하여 Buffer 전체 비웁니다."},
-        {"help", "help", "도움말을 출력합니다."},
-        {"exit", "exit", "셸을 종료합니다."},
-        {"", "", ""},
-        {"test script 1", "1_FullWriteAndReadCompare or 1_", "test script 1을 실행합니다."},
-        {"test script 2", "2_PartialLBAWrite or 2_", "test script 2를 실행합니다."},
-        {"test script 3", "3_WriteReadAging or 3_", "test script 3을 실행합니다."},
-        {"test script 4", "4_EraseAndWriteAging or 4_", "test script 4를 실행합니다."}
-    };
+    {"write", "write [LBA] [Value]", "Write the specified value to the given LBA."},
+    {"read", "read [LBA]", "Read the value from the specified LBA."},
+    {"fullwrite", "fullwrite [Value]", "Write the same value to all LBAs."},
+    {"fullread", "fullread", "Read and print all LBAs."},
+    {"erase", "erase [LBA]  [SIZE]", "Delete values from the specified LBA for the given size."},
+    {"erase_range", "erase_range [Start LBA]  [End LBA]", "Delete values from Start LBA to End LBA."},
+    {"flush", "flush", "Execute all commands in the Command Buffer and clear the entire buffer."},
+    {"help", "help", "Display help information."},
+    {"exit", "exit", "Exit the shell."},
+    {"", "", ""},
+    {"test script 1", "1_FullWriteAndReadCompare or 1_", "Execute test script 1."},
+    {"test script 2", "2_PartialLBAWrite or 2_", "Execute test script 2."},
+    {"test script 3", "3_WriteReadAging or 3_", "Execute test script 3."},
+    {"test script 4", "4_EraseAndWriteAging or 4_", "Execute test script 4."}};
 }
 
 static void HelpPrint(const HelpString& str) {
@@ -38,8 +37,8 @@ static void HelpPrint(const HelpString& str) {
 
 bool CommandHelp::Call(const std::vector<std::string>& program) {
   std::cout << "[Team Info] CRA Double Checker Team\n";
-  std::cout << " - 팀장: 박상환\n";
-  std::cout << " - 팀원: 곽예나, 김연우, 박시은, 양동혁, 정주성\n\n";
+  std::cout << " - Team leader: Sanghwan Park\n";
+  std::cout << " - Team members: Yena Gwak, Yeonwoo Kim, Sieun Park, Donghyuk Yang, Jooseong Jeong\n\n";
 
   std::cout << "[Command Usage]\n";
   for (const auto& str : helpString) {
